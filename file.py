45f491af3b2316c27d43b5d4cbff17eafbb89f79
One Of Your Choices <baatochanass@gmail.com>
2022-05-29 20:50:22 +0100

    Revert "4th commit"
    This reverts commit 0ab6ad13d280de03268606316db14773afd9295a.

--- file.py
+++ file.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 # this modified
 print(" this is 4th modification")
 print(" hi world")
+=======
+# this modified
+>>>>>>> parent of 0ab6ad1 (4th commit)
--- newfile.py
+++ newfile.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 # midification
 print("i am comming python wait for me")
 print(" commit me please")
+=======
+# midification
+>>>>>>> parent of 0ab6ad1 (4th commit)
